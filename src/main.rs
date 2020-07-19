544838964a643106e8eda60b86b903bb80618213
binoy <prakash.binoy@gmail.com>
2020-07-19 12:24:32 -0700

    Turn off echo mode with termios

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use std::io;
+use std::os::unix::io::AsRawFd;
+use termios::*;
 
 fn main() {
     println!("Hello, world!");
+    let stdin = io::stdin();
+    let fd = stdin.as_raw_fd();
+
+    let mut termios = Termios::from_fd(fd).unwrap();
+    termios.c_lflag &= !(ECHO);
+    tcsetattr(fd, TCSANOW, &termios).unwrap();
+
     let mut input = String::new();
     loop {
-        match io::stdin().read_line(&mut input) {
+        match stdin.read_line(&mut input) {
             Ok(n) => {
                 println!("{} bytes read", n);
                 println!("{}", input);
