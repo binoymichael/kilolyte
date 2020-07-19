6e05acc5df1d227f0c8c46e5952cd769f0398c36
binoy <prakash.binoy@gmail.com>
2020-07-19 13:04:00 -0700

    Reading in char by char in canonical mode

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
 use std::io;
+use std::io::Read;
 use std::os::unix::io::AsRawFd;
-use termios::*;
+use termios::{tcsetattr, Termios, ECHO, ICANON, TCSANOW};
 
 fn main() {
     println!("Hello, world!");
-    let stdin = io::stdin();
+
+    // FIXME : Should stdin by mut?
+    let mut stdin = io::stdin();
+
     let fd = stdin.as_raw_fd();
 
     let mut termios = Termios::from_fd(fd).unwrap();
-    termios.c_lflag &= !(ECHO);
+    termios.c_lflag &= !(ECHO | ICANON);
     tcsetattr(fd, TCSANOW, &termios).unwrap();
 
-    let mut input = String::new();
+    let mut character = [0];
     loop {
-        match stdin.read_line(&mut input) {
+        match stdin.read(&mut character) {
             Ok(n) => {
                 println!("{} bytes read", n);
-                println!("{}", input);
+                println!("{:?}", character[0] as char);
             }
             Err(error) => println!("{}", error),
         }
