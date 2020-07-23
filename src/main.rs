178195ffc004ed96f8141ea9302e596142fa4dea
binoy <prakash.binoy@gmail.com>
2020-07-22 21:03:21 -0700

    clean up code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-use std::io::{self, Read, Write};
-use std::os::unix::io::AsRawFd;
+use std::io::{self, Read, Stdin, Write};
+use std::os::unix::io::{AsRawFd, RawFd};
 use termios::{tcsetattr, Termios, ECHO, ICANON, ICRNL, IEXTEN, ISIG, OPOST, TCSANOW, VMIN, VTIME};
 
 fn main() {
-    println!("Hello, world!");
-
-    // FIXME : Should stdin by mut?
     let mut stdin = io::stdin();
+    enable_raw_mode(&stdin);
 
+    loop {
+        let c = editor_key(&mut stdin);
+        match c {
+            '\x00' => (),
+            '\x11' => break,
+            _ => {
+                print!("{}", c);
+                io::stdout().flush().unwrap();
+            }
+        }
+    }
+}
+
+fn editor_key(stdin: &mut Stdin) -> char {
+    let mut character = [0];
+    &stdin.read(&mut character);
+    character[0] as char
+}
+
+fn enable_raw_mode(stdin: &Stdin) {
     let fd = stdin.as_raw_fd();
-
-    // FIXME : What does unwrap do?
     let mut termios = Termios::from_fd(fd).unwrap();
     termios.c_lflag &= !(ECHO | ICANON | ISIG | IEXTEN);
     termios.c_iflag &= !(ICRNL);
@@ -18,20 +34,4 @@ fn main() {
     termios.c_cc[VMIN] = 0;
     termios.c_cc[VTIME] = 1;
     tcsetattr(fd, TCSANOW, &termios).unwrap();
-
-    // FIXME : What is happening here?
-    let mut character = [0];
-    loop {
-        character[0] = 0;
-        match stdin.read(&mut character) {
-            Ok(_) => {
-                print!("{:?}\r\n", character[0] as char); // FIXME: How does 'as char' work?
-                if character[0] == 17 {
-                    break;
-                }
-            }
-            Err(error) => print!("{}\r\n", error),
-        }
-        io::stdout().flush().unwrap();
-    }
 }
