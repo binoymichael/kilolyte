e231ad950e623dec8a4de2291567ee8d38cb41b9
binoy <prakash.binoy@gmail.com>
2020-07-22 21:15:16 -0700

    Switching to use crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
+use crossterm::terminal;
 use std::io::{self, Read, Stdin, Write};
-use std::os::unix::io::{AsRawFd, RawFd};
-use termios::{tcsetattr, Termios, ECHO, ICANON, ICRNL, IEXTEN, ISIG, OPOST, TCSANOW, VMIN, VTIME};
 
-fn main() {
+fn main() -> Result<(), crossterm::ErrorKind> {
+    terminal::enable_raw_mode()?;
     let mut stdin = io::stdin();
-    enable_raw_mode(&stdin);
-
     loop {
         let c = editor_key(&mut stdin);
         match c {
@@ -17,21 +15,12 @@ fn main() {
             }
         }
     }
+    terminal::disable_raw_mode()?;
+    Ok(())
 }
 
 fn editor_key(stdin: &mut Stdin) -> char {
     let mut character = [0];
     &stdin.read(&mut character);
     character[0] as char
-}
-
-fn enable_raw_mode(stdin: &Stdin) {
-    let fd = stdin.as_raw_fd();
-    let mut termios = Termios::from_fd(fd).unwrap();
-    termios.c_lflag &= !(ECHO | ICANON | ISIG | IEXTEN);
-    termios.c_iflag &= !(ICRNL);
-    termios.c_oflag &= !(OPOST);
-    termios.c_cc[VMIN] = 0;
-    termios.c_cc[VTIME] = 1;
-    tcsetattr(fd, TCSANOW, &termios).unwrap();
 }
